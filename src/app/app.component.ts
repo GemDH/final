7f01577a92ea6bcc1290e76a45dce040a592d5b4
kate <gemariecoritanamatobato@gmail.com>
2022-01-08 12:48:48 +0800

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'gemfinal';
+
+  result: number = 0;
+  num1: number = 0;
+  num2: number = 0;
+
+  add() {
+    this.result = this.num1 + this.num2;
+  }
+
+  sub() {
+    this.result = this.num1 - this.num2;
+  }
+
+  mul() {
+    this.result = this.num1 * this.num2;
+  }
+
+  div() {
+    this.result = this.num1 / this.num2;
+  }
 }
